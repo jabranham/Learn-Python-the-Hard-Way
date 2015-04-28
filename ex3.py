92f7d5046ff1c16f4408b635459fc23438f47dcf
Alex Branham <alex.branham@gmail.com>
2015-04-27 21:18:57 -0500

    ex 3 study drills

--- ex3.py
+++ ex3.py
@@ -1,23 +1,27 @@
+# prints command
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100-25 * 3 % 4
+# Counts my chickens 25 + (30/6)
+print "Hens", 25.0 + 30.0 / 6.0
+# Counts my roosters (25*3, then divided by 4 has 3 remaining, so 97 roosters)
+print "Roosters", 100.0-25.0 * 3.0 % 4.0
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# PEMDAS 
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
 
 print "Oh, that's why it's False."
 
 print "How about some more."
 
-print "Is it greater?", 5 > -2
-print "is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <=-2
+print "Is it greater?", 5.0 > -2.0
+print "is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <=-2.0
